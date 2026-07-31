33462175768a41aea178f13a6c304e98f6833fcd
mch55873-arch <mch55873@gmail.com>
2026-07-31 08:59:48 +0500

    Deploy 1:1 Mold Replica & Critical Sitemap Routing Fix for cantreeservice.com

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,5 @@
-import { articles } from "../data/articles";
-import { services } from "../data/services";
+import articles from "../data/articles.json";
+import services from "../data/services.json";
 import usaLocations from "../data/usa_database.json";
 import {
   aboutUsPage,
